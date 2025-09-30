b020d064b66316087a9ca1ca6c2a58ed43924a1d
Kamlesh Kumar <kkumar.bscs20seecs@seecs.edu.pk>
2025-09-30 14:39:25 +0300

    Ex: 1.1: Course Information

---
+++ Part_1/courseinfo/src/App.jsx
@@ -0,0 +1,43 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    props.parts.map((part) =>
+    (
+      <p >{part.name} {part.exercises} </p>
+    )
+    )
+  )
+}
+
+const Total = (props) => {
+
+  let totalExcercises = 0
+
+  props.parts.map((part) => {
+    totalExcercises += part.exercises
+  })
+
+  return <p>Number of exercises {totalExcercises}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App;
